b5b32614908c4ac5de463efdd0cc738d03eef184
RosiePosie24 <rosfanida24@gmail.com>
2026-05-03 21:46:35 +0800

    Create Encoded class with data fields

---
+++ Encoded.java
@@ -0,0 +1,9 @@
+public class Encoded {
+//Data field- to store information
+  private String inputText;  //the string that the user wants to encode
+  private int charCount;    //the number of non-space characters in inputText
+  private String result
+
+
+  
+}
