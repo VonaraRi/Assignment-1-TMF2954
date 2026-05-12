2a8f32d1883eeb6b3c8e45dbd630a99d7a011817
VonaraRi <rionnalynrichard5@gmail.com>
2026-05-12 10:05:38 +0800

    The main class

--- Assignment.java
+++ Assignment.java
@@ -1,16 +1,23 @@
+/* This is the driver class for the encoder system
+ * It initializes the main application window and hosts the assignmentPanel
+ * Contributed by Rionnalyn
+ */
+
 import javax.swing.JFrame;
 
 public class Assignment{
     public static void main(String[] args){
-        JFrame frame = new JFrame("TMF2964 Assignment 1");
+        //Create new window frame with a title and ensure the application exits when window is closed
+        JFrame frame = new JFrame("TMF2964 Assignment 1 : ENCODER SYSTEM");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
 
-        //adding the custom panel
+        // Adding the custom panel containing the GUI components and logic
         assignmentPanel panel = new assignmentPanel();
 
-        frame.getContentPane().add(panel);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
+        frame.getContentPane().add(panel); // Add the panel to the frame's content pane
+        frame.pack(); // Adjust the window size
+        frame.setVisible(true); // Make the window visivle to the user
+        frame.setLocationRelativeTo(null); // Set the window location to center window on screen
     }
 }
